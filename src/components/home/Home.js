fedf43fffa871d6e48e0b4e9bb153da73b7014d3
Aditya8766 <111683825+Aditya8766@users.noreply.github.com>
2024-03-14 08:21:13 +0530

    updated-code

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,8 +4,53 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import myPic from '../../assets/mypic.png';
 import Typewriter from "typewriter-effect";
+import { useEffect, useRef } from "react";
 
 const Home = () => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if(canvas){
+        const ctx = canvas.getContext("2d");
+    
+        const lines = [];
+        const numLines = 50;
+    
+        for (let i = 0; i < numLines; i++) {
+          lines.push({
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height,
+            speed: Math.random() * 10 + 5,
+          });
+        }
+    
+        const draw = () => {
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+          lines.forEach((line) => {
+            line.x += line.speed;
+    
+            // Reset the line position when it goes out of the canvas
+            if (line.x > canvas.width) {
+              line.x = 0;
+            }
+    
+            ctx.beginPath();
+            ctx.moveTo(line.x, line.y);
+            ctx.lineTo(line.x + 5, line.y + 5);
+            ctx.strokeStyle = "white";
+            ctx.lineWidth = 2;
+            ctx.stroke();
+          });
+    
+          requestAnimationFrame(draw);
+        };
+    
+        draw();
+    }
+
+  }, []);
   return (
     <><div className="main-container">
       <Header />
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,8 +1,8 @@
 import './projects.scss';
 import app from '../../assets/app.png';
-import cart from '../../assets/cart.png';
+import docAnalyzer from '../../assets/docAnalyzer.png';
 import movie from '../../assets/movie.png';
-import rental from '../../assets/rental.png';
+import chatRooms from '../../assets/chatRooms.png';
 const Projects=()=>{
     return(
         <div className="projects-container" id="projects">
@@ -14,19 +14,19 @@ const Projects=()=>{
                         <a href='https://github.com/Aditya8766/movie-search-filter-react'>checkout</a>
                     </div>
                     <div className="cards">
-                    <img src={cart} alt='shopping-cart-website-img'/>
-                        <h3>React Shopping Cart</h3>
-                        <a href='https://github.com/Aditya8766/react_cart/tree/main'>checkout</a>
+                        <img src={docAnalyzer} alt='doc-analyzer-img'/>
+                        <h3>Document Analyzer</h3>
+                        <a href='https://github.com/Aditya8766/DocumentAnalyzer'>checkout</a>
+                    </div>
+                    <div className="cards">
+                    <img src={chatRooms} alt='chat-rooms-img'/>
+                        <h3>Chat Rooms</h3>
+                        <a href='https://github.com/Aditya8766/ChatRooms'>checkout</a>
                     </div>
                     <div className="cards">
                     <img src={app} alt='app-img'/>
                         <h3>React Native Frontend</h3>
                         <a href='https://github.com/Aditya8766/ReactNativeCourceFromMetaAssisment'>checkout</a>
-                    </div>
-                    <div className="cards">
-                    <img src={rental} alt='rental-website-img'/>
-                        <h3>Online Rental System</h3>
-                        <a href='https://github.com/Aditya8766/onllineRentalSystem'>checkout</a>
                     </div>
                 </div>
                 <div className="divider"></div>
